c31cfc70bab1542d4a6a336cc92d921dd3c85132
Mariem BenYounes <maryemby21@gmail.com>
2024-03-18 15:44:56 +0100

    making the matrix diag dominant

--- Python-Implementation/GaussSeidel.py
+++ Python-Implementation/GaussSeidel.py
@@ -1,13 +1,33 @@
 import numpy as np
 
-
 def generate_diagonally_dominant(matrix,size):
     for i in range(size):
         row_sum = np.sum(np.abs(matrix[i, :])) - np.abs(matrix[i, i]) 
         matrix[i, i] += row_sum * 1.1  
     return matrix
 
-def GS(matrix, n, b, tolerance=1e-8,omega=1):
+
+def GS(matrix, n, b, tolerance=1e-8, omega=1):
+    matrix=generate_diagonally_dominant(matrix,n) #ensures that the matrix will converge
+    sol = np.zeros(n)
+    it = 0
+    while True:
+        it += 1
+        prev = sol.copy()
+        for j in range(n):
+            sum = 0
+            for k in range(n):
+                if k != j:
+                    sum += sol[k] * matrix[j, k]
+            sol[j] = omega*(b[j] - sum) / matrix[j, j] + (1-omega)*sol[j]
+        if np.allclose(sol, prev, atol=tolerance, rtol=0):
+            print(f"Solution converged in {it} iterations.")
+            break
+    return sol
+
+#added here error messages
+def GS2(matrix, n, b, tolerance=1e-8,omega=1):
+    matrix=generate_diagonally_dominant(matrix,n)
     sol = np.zeros(n)
     it = 0
     try:
@@ -32,3 +52,10 @@ def GS(matrix, n, b, tolerance=1e-8,omega=1):
         print(f"Iterations completed: {it}")
 
 
+
+size = 1000
+matrix = np.random.rand(size, size)
+
+b = np.random.rand(size)
+
+GS(matrix, size, b)
