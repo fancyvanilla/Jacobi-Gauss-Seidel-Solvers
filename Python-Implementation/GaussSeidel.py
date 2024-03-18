50f1856f27035a9d75fab96048111ba460453e0c
Mariem BenYounes <maryemby21@gmail.com>
2024-03-18 17:37:48 +0100

    deleted GS2

--- Python-Implementation/GaussSeidel.py
+++ Python-Implementation/GaussSeidel.py
@@ -7,27 +7,8 @@ def generate_diagonally_dominant(matrix,size):
     return matrix
 
 
-def GS(matrix, n, b, tolerance=1e-8, omega=1):
+def GS(matrix, n, b, tolerance=1e-8,omega=1):
     matrix=generate_diagonally_dominant(matrix,n) #ensures that the matrix will converge
-    sol = np.zeros(n)
-    it = 0
-    while True:
-        it += 1
-        prev = sol.copy()
-        for j in range(n):
-            sum = 0
-            for k in range(n):
-                if k != j:
-                    sum += sol[k] * matrix[j, k]
-            sol[j] = omega*(b[j] - sum) / matrix[j, j] + (1-omega)*sol[j]
-        if np.allclose(sol, prev, atol=tolerance, rtol=0):
-            print(f"Solution converged in {it} iterations.")
-            break
-    return sol
-
-#added here error messages
-def GS2(matrix, n, b, tolerance=1e-8,omega=1):
-    matrix=generate_diagonally_dominant(matrix,n)
     sol = np.zeros(n)
     it = 0
     try:
@@ -51,11 +32,3 @@ def GS2(matrix, n, b, tolerance=1e-8,omega=1):
         print(f"Error occurred: {e}")
         print(f"Iterations completed: {it}")
 
-
-
-size = 1000
-matrix = np.random.rand(size, size)
-
-b = np.random.rand(size)
-
-GS(matrix, size, b)
