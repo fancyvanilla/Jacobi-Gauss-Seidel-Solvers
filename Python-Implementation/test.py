f5ca875768ffdfd5ca4a32fe141306a01809f0d3
Mariem BenYounes <maryemby21@gmail.com>
2024-03-18 15:47:20 +0100

    added a test_method

--- Python-Implementation/test.py
+++ Python-Implementation/test.py
@@ -1,64 +1,54 @@
 import numpy as np
 import time
-import matplotlib.pyplot as plt
 from GaussSeidel import GS
 from Jacobi import Jacobi
+import matplotlib.pyplot as plt
 
 
-matrix_sizes=[100, 400, 500, 700, 1000, 1500 ,2000]
-avg_times_Jacobi=[]
-avg_times_GS=[]
+def test_method(method, matrix_sizes):
+    avg_times = []
 
-for size in matrix_sizes:
-    runtimes1 = []
-    runtimes2 = []
-    b = np.ones(size)
-    matrix = np.random.rand(size, size)
+    for size in matrix_sizes:
+        runtimes = []
+        b = np.ones(size)
+        matrix = np.random.rand(size, size)
 
+        for _ in range(5): 
+            start_time = time.time()
 
-    for _ in range(5): 
-        #Jacobi
-        start_time = time.time()
+            method(matrix, size, b)
 
-        Jacobi(matrix,size,b)
+            end_time = time.time()
 
-        end_time = time.time()
+            runtime = end_time - start_time
+            runtimes.append(runtime)
 
-        runtime = end_time - start_time
-        runtimes1.append(runtime)
-        #Seidel
-        start_time = time.time()
+        avg_times.append(np.mean(runtimes))
 
-        GS(matrix,size,b)
+    return avg_times
 
-        end_time = time.time()
+def main():
+    matrix_sizes=[100, 400, 500, 700, 1000, 1500 ,2000]
 
-        runtime = end_time - start_time
-        runtimes2.append(runtime)
+    avg_times_Jacobi = test_method(Jacobi, matrix_sizes)
+    avg_times_GS = test_method(GS, matrix_sizes)
 
+    x = matrix_sizes
+    #y1 = avg_times_Jacobi
+    y2 = avg_times_GS
 
-    avg_runtime = sum(runtimes1) / 5
-    avg_times_Jacobi.append(avg_runtime)
+    #smoothed_y1 = np.convolve(y1, np.ones(3), mode='same') / 3
+    smoothed_y2 = np.convolve(y2, np.ones(3), mode='same') / 3
 
-    avg_runtime = sum(runtimes2) / 5
-    avg_times_GS.append(avg_runtime)
+    #plt.plot(x, smoothed_y1, label="Données lissées Jacobi")
+    plt.plot(x, smoothed_y2, label="Données lissées Gauss-Seidel")
 
+    plt.legend()
+    plt.xlabel("Taille de la matrice")
+    plt.ylabel("Temps d'exécution (secondes)")
 
-x = matrix_sizes
-y1 = avg_times_Jacobi
-y2 = avg_times_GS
-
-
-
-smoothed_y1 = np.convolve(y1, np.ones(3), mode='same') / 3
-smoothed_y2 = np.convolve(y2, np.ones(3), mode='same') / 3
+    plt.show()
 
 
-plt.plot(x, smoothed_y1, label="Données lissées Jacobi")
-plt.plot(x, smoothed_y2, label="Données lissées Gauss-Seidel")
-
-plt.legend()
-plt.xlabel("Taille de la matrice")
-plt.ylabel("Temps d'exécution (secondes)")
-
-plt.show()
+if __name__ == "__main__":
+    main()
