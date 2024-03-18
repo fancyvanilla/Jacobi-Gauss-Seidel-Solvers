8040903d3a895ddd4805d0c833f06b01c7a019fe
Mariem BenYounes <maryemby21@gmail.com>
2024-03-18 17:33:43 +0100

    removed a function

--- Python-Implementation/Jacobi.py
+++ Python-Implementation/Jacobi.py
@@ -1,13 +1,9 @@
 import numpy as np
-
-
-def normalize(matrix):
-    max_val = np.max(np.abs(matrix))
-    return matrix / max_val
+from GaussSeidel import generate_diagonally_dominant
 
 
 def Jacobi(matrix, n, b, tolerance=1e-8, omega=1):
-    matrix=normalize(matrix)
+    matrix=generate_diagonally_dominant(matrix,n)
     sol = np.zeros(n)
     it = 0
     try:
@@ -26,12 +22,9 @@ def Jacobi(matrix, n, b, tolerance=1e-8, omega=1):
                 curr.append(omega*(b[j] - sum) / matrix[j, j] + (1-omega)*sol[j])
             if np.allclose(sol, curr, atol=tolerance, rtol=0):
                 print(f"Solution converged in {it} iterations.")
-                print(sol)
                 break
             sol=curr
             curr=[]
     except Exception as e:
         print(f"Error occurred: {e}")
         print(f"Iterations completed: {it}")
-
-
